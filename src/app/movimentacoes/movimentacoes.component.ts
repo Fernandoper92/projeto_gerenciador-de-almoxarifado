25f43eb7cd48e1b152baee28a84a9ae3e00f8a37
Fernando Pereira <fernandoper92@gmail.com>
2021-05-14 04:09:22 -0300

    esconder botões de imprimir e filtro

--- src/app/movimentacoes/movimentacoes.component.ts
+++ src/app/movimentacoes/movimentacoes.component.ts
@@ -280,15 +280,4 @@ export class MovimentacoesComponent implements OnInit {
       });
     }
   }
-
-  printDiv() {
-    var printContents = document.getElementById('printableArea').innerHTML;
-    var originalContents = document.body.innerHTML;
-
-    document.body.innerHTML = printContents;
-
-    window.print();
-
-    document.body.innerHTML = originalContents;
-  }
 }
